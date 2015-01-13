3fe83043c7dcf2920b2301f7883a8b7110e668a2
nagacharan <nagacharan09@gmail.com>
2015-01-13 19:38:37 +0530

    initial

---
+++ firstjsfapplication/src/main/java/com/pkrm/first/Employee.java
@@ -0,0 +1,102 @@
+package com.pkrm.first;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+@ManagedBean(name = "employee")
+public class Employee implements Serializable {
+
+	private static Map<String, Employee> employees = new TreeMap<String, Employee>();
+	private String id;
+	private String name;
+	private String department;
+	private int age;
+	private double salary;
+	private String email;
+
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(String name, String department, int age, double salary) {
+		this.name = name;
+		this.department = department;
+		this.age = age;
+		this.salary = salary;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String submit() {
+		Employee employee = this;
+		System.out.println("Name:::...!!" + employee.getName());
+		employees.put(employee.getId(), employee);
+		return "EmployeeList";
+	}
+
+	public List<Employee> getEmployees() {
+		return new ArrayList<Employee>(employees.values());
+	}
+
+	public Employee getEmployee(int id) {
+		return employees.get(id);
+	}
+
+	public String addEmployee() {
+		System.out.println("addEmployee");
+		return "employee";
+	}
+
+}
---
+++ firstjsfapplication/src/main/java/com/pkrm/first/FirstJsf.java
@@ -0,0 +1,24 @@
+package com.pkrm.first;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "firstJsf", eager = true)
+public class FirstJsf {
+	
+	private String message;
+	
+	   public FirstJsf() {
+	      System.out.println("FirstJsf application!");
+	      
+	      this.message="FirstJsf application";
+	   }
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	   
+	}
